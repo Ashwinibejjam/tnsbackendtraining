33488ff2fe380efa0c6ebd5e2e37d006f047dc5c
Lenovo <Lenovo@DESKTOP-4N5AC4Q>
2025-08-27 16:57:03 +0530

    initial commit

--- main/tnsbackendtraining/src/tnsbackendtraining/ReverseArray.java
+++ main/tnsbackendtraining/src/tnsbackendtraining/ReverseArray.java
@@ -4,9 +4,9 @@ public class ReverseArray {
 	public void reverse(int[] arr) {
 		System.out.print("Reversed Array:");
 		for(int i=arr.length-1;i>=0;i--) {
-		  System.out.print(arr[i] + " ");
+		System.out.print(arr[i] + " ");
 		
 		}
 		System.out.println();
-	}	
+	}
 }
--- main/tnsbackendtraining/src/tnsbackendtraining/ReverseArraymain.java
+++ main/tnsbackendtraining/src/tnsbackendtraining/ReverseArraymain.java
@@ -3,10 +3,9 @@ package tnsbackendtraining;
 public class ReverseArraymain {
 
 	public static void main(String[] args) {
-		int[] arr = {10,20,30,40,50};
-		ReverseArray obj = new ReverseArray();
-		obj.reverse(arr);
+			int[] arr = {10,20,30,40,50};
+			ReverseArray obj = new ReverseArray();
+			obj.reverse(arr);
 		
+		}
 	}
-
-}
