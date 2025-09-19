29d3a66de549920b0d93581f0b082c7bbf2f442c
Lenovo <Lenovo@DESKTOP-4N5AC4Q>
2025-09-19 15:16:27 +0530

    initial commit

---
+++ main/tnsbackendtraining/src/tnsbackendtraining/SortArray.java
@@ -0,0 +1,10 @@
+package tnsbackendtraining;
+import java.util.Arrays;
+public class SortArray {
+	public static void main (String args[]) {
+        int[] arr = {5, 2, 8, 7, 1};
+        Arrays.sort(arr);
+        System.out.println("Sorted: " + Arrays.toString(arr));
+
+	}
+}
